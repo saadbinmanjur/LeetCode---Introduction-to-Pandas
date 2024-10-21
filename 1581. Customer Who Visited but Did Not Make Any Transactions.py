f58b0a80e11291b689cde51b0be68c05a5c558a0
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-21 06:56:41 +0600

    pandas-easy

--- 1581. Customer Who Visited but Did Not Make Any Transactions.py
+++ 1581. Customer Who Visited but Did Not Make Any Transactions.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 def find_customers(visits: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
     
+    df = pd.merge(visits, transactions, on = 'visit_id', how = 'left')
+    df = df[df['transaction_id'].isna()]
+    df = df.groupby(['customer_id'])['visit_id'].count().reset_index()
+
+    return df.rename({'visit_id':'count_no_trans'}, axis = 1)
