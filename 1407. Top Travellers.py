b675d5f19a64e25967b6b6f58c7a5c1f8a62c69a
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-22 06:25:08 +0600

    pandas-easy

--- 1407. Top Travellers.py
+++ 1407. Top Travellers.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def top_travellers(users: pd.DataFrame, rides: pd.DataFrame) -> pd.DataFrame:
-    
+    df = rides.groupby(['user_id'])['distance'].sum().reset_index(name='travelled_distance')
+    df = pd.merge(users, df, how = 'left', left_on = 'id', right_on = 'user_id')
+    df['travelled_distance'] = df['travelled_distance'].fillna(0)
+    df.sort_values(by = ['travelled_distance', 'name'], ascending = [False, True], inplace = True)
+    return df[['name', 'travelled_distance']]
