c3cc088bd80d09ebf5fd1d1af4400001e049fc69
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-23 06:44:44 +0600

    pandas-easy

--- 1587. Bank Account Summary II.py
+++ 1587. Bank Account Summary II.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def account_summary(users: pd.DataFrame, transactions: pd.DataFrame) -> pd.DataFrame:
-    
+    df = pd.merge(users, transactions, on='account', how='inner')
+    df = df.groupby(['account', 'name'])[['amount']].sum().reset_index()
+    df = df.loc[df.amount > 10000][['name', 'amount']]
+    df.rename(columns = {'amount': 'balance'}, inplace=True)
+    return df
