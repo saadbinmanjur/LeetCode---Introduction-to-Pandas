4245375c6501854cfaa8a2e63266f46480f4541a
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-25 09:05:37 +0600

    pandas-easy

--- 1661. Average Time of Process per Machine.py
+++ 1661. Average Time of Process per Machine.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
-    
+    pivoted = activity.pivot(index=["machine_id", "process_id"], columns='activity_type', values='timestamp')
+    pivoted['processing_time'] = pivoted['end'] - pivoted['start']
