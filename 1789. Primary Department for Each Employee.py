1f8395d26f639b6fd54beedd36e31af58a22890d
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-27 06:14:24 +0600

    pandas-easy

--- 1789. Primary Department for Each Employee.py
+++ 1789. Primary Department for Each Employee.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
-    
+    df = employee[(~employee.duplicated(subset='employee_id', keep=False)) | (employee.primary_flag=='Y')]
+    return df[['employee_id', 'department_id']]
