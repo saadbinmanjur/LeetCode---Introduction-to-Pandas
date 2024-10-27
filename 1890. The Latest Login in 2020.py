0ec92ec9f7a4baa0378df7e985a8188be8cb3b99
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-27 06:41:12 +0600

    pandas-easy

--- 1890. The Latest Login in 2020.py
+++ 1890. The Latest Login in 2020.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def latest_login(logins: pd.DataFrame) -> pd.DataFrame:
-    
+    return logins.loc[logins.time_stamp.dt.year == 2020].groupby('user_id').max().reset_index().rename(columns={'time_stamp':'last_stamp'})
