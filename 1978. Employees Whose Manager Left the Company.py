867c03abc8fff71cb6b4cb93dee9c46f21a786fe
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-29 07:06:18 +0600

    pandas-easy

--- 1978. Employees Whose Manager Left the Company.py
+++ 1978. Employees Whose Manager Left the Company.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def find_employees(employees: pd.DataFrame) -> pd.DataFrame:
-    
+    df = employees.loc[(employees.salary < 30000) & (~employees.manager_id.isnull()),['manager_id', 'employee_id']]
+    df = df.loc[~df['manager_id'].isin(employees.employee_id), ['employee_id']].sort_values(by='employee_id')
+    return df
