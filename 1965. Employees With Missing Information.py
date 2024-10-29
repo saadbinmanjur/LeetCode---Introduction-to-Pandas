a86daff45010a7b3cf53ce6c8eb0615b7ab594ce
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-29 06:51:54 +0600

    pandas-easy

--- 1965. Employees With Missing Information.py
+++ 1965. Employees With Missing Information.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def find_employees(employees: pd.DataFrame, salaries: pd.DataFrame) -> pd.DataFrame:
-    
+    return pd.DataFrame({'employee_id': sorted(set(employees.employee_id) ^
+                                                set(salaries.employee_id))})
