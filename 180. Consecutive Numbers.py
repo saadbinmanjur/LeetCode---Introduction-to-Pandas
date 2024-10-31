850919823e7227fd38ed6a0dd33bf7b721a7201a
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-10-31 06:23:44 +0600

    pandas-medium

--- 180. Consecutive Numbers.py
+++ 180. Consecutive Numbers.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
 def consecutive_numbers(logs: pd.DataFrame) -> pd.DataFrame:
-    
+    logs['var'] = logs.num.rolling(window = 3).var()
+    return pd.DataFrame(data={'ConsecutiveNums': logs.query('var == 0').num.unique()})
