7d55f73371bfadc631c23fa4d56cd92ae62d7ba4
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-04 06:54:07 +0600

    pandas-medium

--- 608. Tree Node.py
+++ 608. Tree Node.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 def tree_node(tree: pd.DataFrame) -> pd.DataFrame:
+    tree = tree.where(pd.notnull(tree), -1)
+    ids, p_ids, nodes = tree['id'].to_list(),tree['p_id'].to_list(), []
+
+    for i in range(len(ids)):
+        if p_ids[i] == -1   : nodes.append("Root")
+        elif ids[i] in p_ids: nodes.append("Inner")
+        else                : nodes.append("Leaf")
     
+    return pd.DataFrame({'id':ids, 'type':nodes})
