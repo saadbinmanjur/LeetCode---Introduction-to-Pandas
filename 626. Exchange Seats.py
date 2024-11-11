f1970aa44a3dfc629ba46675e2f45c5ef9992452
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-11-11 06:21:23 +0600

    pandas-medium

--- 626. Exchange Seats.py
+++ 626. Exchange Seats.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 def exchange_seats(seat: pd.DataFrame) -> pd.DataFrame:
-    
+    l = len(seat)
+    for i in range(1, l, 2):
+        seat.iloc[i-1, 1], seat.iloc[i, 1] = seat.iloc[i, 1], seat.iloc[i-1, 1]
+    return seat
