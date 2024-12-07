593cfd1fedef8d091956250f61a617244d560f5c
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-12-07 12:35:12 +0600

    pandas-medium

--- 1070. Product Sales Analysis III.py
+++ 1070. Product Sales Analysis III.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
-    
+    sales['first_year'] = sales.groupby('product_id')[['year']].transform('min')
+    sales = sales[sales.first_year == sales.year]
+    return sales[['product_id', 'first_year', 'quantity', 'price']]
