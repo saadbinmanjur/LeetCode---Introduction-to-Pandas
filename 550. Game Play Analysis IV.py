18fe3c88c0ad2846ce954e55d053ff118c0367a2
saadbinmanjur <saadbinmanjuradit@gmail.com>
2024-12-08 06:46:13 +0600

    pandas-medium

--- 550. Game Play Analysis IV.py
+++ 550. Game Play Analysis IV.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 def gameplay_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    
+    activity["first_day"] = activity.groupby(["player_id"])[["event_date"]].transform("min")
+    activity["is_day"] = (activity.event_date - activity.first_day).dt.total_seconds() == 86400
+    df = activity[activity["is_day"]]
+    return pd.DataFrame({"fraction":[round(df.player_id.nunique() / activity.player_id.nunique(),2)]})
