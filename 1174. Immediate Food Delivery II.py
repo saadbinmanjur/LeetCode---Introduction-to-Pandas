6167cf3efc37c8ea37dd2704e0c9ea74dcd87621
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-01-25 11:20:43 +0600

    pandas-medium

--- 1174. Immediate Food Delivery II.py
+++ 1174. Immediate Food Delivery II.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
-    
+    df=delivery.groupby("customer_id").min()
+    df1=df[df["order_date"]==df["customer_pref_delivery_date"]]
+    return pd.DataFrame({"immediate_percentage":[len(df1)/len(df)*100]}).round(2)
