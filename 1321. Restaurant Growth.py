5b978376643fa014ed31d2d8f3b9389d049af97f
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-01-28 11:29:34 +0600

    pandas-medium

--- 1321. Restaurant Growth.py
+++ 1321. Restaurant Growth.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def restaurant_growth(customer: pd.DataFrame) -> pd.DataFrame:
-    
+    df = customer.sort_values("visited_on").groupby("visited_on")[["amount"]].sum()
+    df = df.assign(amount = df.rolling("7D").sum(), average_amount = round(df.rolling("7D").sum()/7,2))
+    return df.loc[df.index >= df.index.min() + pd.DateOffset(6)].reset_index()
