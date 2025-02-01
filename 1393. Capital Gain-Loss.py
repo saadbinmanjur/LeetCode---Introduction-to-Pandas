dd15d9ca427753dad67493ec04919203b194901a
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-02-01 09:48:23 +0600

    pandas-medium

--- 1393. Capital Gain-Loss.py
+++ 1393. Capital Gain-Loss.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def capital_gainloss(stocks: pd.DataFrame) -> pd.DataFrame:
-    
+    stocks['operation'] = stocks['operation'].apply(lambda x: -1 if x == 'Buy' else 1)
+    stocks['price'] *= stocks['operation']
+    return stocks.groupby("stock_name")["price"].sum().reset_index().rename(columns={"price":"capital_gain_loss"})
