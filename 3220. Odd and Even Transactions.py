bb5d6e59d8d64abc4f6ecfa62aa246a7444671cb
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-02-03 20:12:28 +0600

    pandas-medium

--- 3220. Odd and Even Transactions.py
+++ 3220. Odd and Even Transactions.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 def sum_daily_odd_even(transactions: pd.DataFrame) -> pd.DataFrame:
     
+    odds  = transactions[transactions.amount%2 == 1
+             ].groupby(['transaction_date']).sum().reset_index()
+
+    evens = transactions[transactions.amount%2 == 0
+             ].groupby(['transaction_date']).sum().reset_index()
+
+    return  pd.merge(odds, evens, how = 'outer', on = 'transaction_date'
+             ).sort_values('transaction_date').fillna(0).iloc[:,[0,2,4]
+             ].rename(columns = {'amount_x':'odd_sum', 'amount_y':'even_sum'})
