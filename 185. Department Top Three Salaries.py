2280e7478ad2d04e835b38c7747191bd31694ddd
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-02-05 15:42:18 +0600

    pandas-hard

--- 185. Department Top Three Salaries.py
+++ 185. Department Top Three Salaries.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-    
+def top_three_salaries(employee: pd.DataFrame, 
+                       department: pd.DataFrame) -> pd.DataFrame:
+
+    employee.  columns = ['id', 'Employee', 'Salary', 'd_id']
+    department.columns = ['d_id', 'Department']
+
+    employee['rnk'] = employee.groupby('d_id')[['Salary']
+                             ].rank(method='dense', ascending=False)
+
+    return employee[employee.rnk <= 3
+                ].merge(department, on='d_id').iloc[:,[5,1,2]]
