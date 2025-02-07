ecd727d5d5161b7e39a5c909ee1ebc1ffa59934c
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-02-07 12:05:08 +0600

    pandas-hard

--- 601. Human Traffic of Stadium.py
+++ 601. Human Traffic of Stadium.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 def human_traffic(stadium: pd.DataFrame) -> pd.DataFrame:
-    
+
+    adj = lambda x: stadium.id.diff().shift(x) == 1
+
+    stadium = stadium[stadium.people > 99]
+
+    return stadium[adj(0) & adj(1) | adj(-1) & adj(0) | adj(-2) & adj(-1)]
